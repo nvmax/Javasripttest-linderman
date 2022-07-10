f3995d8e3be76a9efb56ec639918e1f85cbd507d
Jerrod Linderman <nvmaxx@gmail.com>
2022-07-10 16:39:48 -0500

    added reset of the functions updated notes to code and refactored some code for better understanding, updated readme.MD

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Javasripttest-linderman
+<div id="header" align="center">
+  <img src="https://media.giphy.com/media/M9gbBd9nbDrOTu1Mqx/giphy.gif" width="100"/>
+</div>
+<div align="center">
+ Jerrod Linderman
+</div>
+<div align="center">
+<img src="https://komarev.com/ghpvc/?username=nvmaxx&style=flat-square&color=blue" alt=""/>
+</div>
+
+## JavaScript Quiz  
+Using javascript to create a quiz.
+
+Creates quiz with dynamic elements with in the html using javascript. 
+
+Buttons, questions and answers are created dynamically in the html from javascript. 
+All questions and answers are stored in an array.
+
+All functions are tested and work as intended.
+
+Start screen :
+
+![](/assets/images/javateststart.png)
+
+First question :
+
+![](/assets/images/testquestion.png)
+
+savescore screen :
+
+![](/assets/images/savescore.png)
+
+viewing scores screen :
+
+![](/assets/images/viewingscores.png)
+
+
+
+
+
+repo: https://github.com/nvmax/Javasripttest-linderman
+
+deployed site:  https://nvmax.github.io/Javasripttest-linderman/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
+// vararibles used with in functions and page.
 let secondsLeft = 60;
-let emptyArray = [];
-let buttonsDiv = document.getElementById("buttons")
 let timer = document.getElementById("timer");
+let buttonsDiv = document.getElementById("buttons");
 let startButton = document.getElementById("start-button");
-startButton.addEventListener("click", setTime);
-var questionDiv = document.getElementById("question-div");
+var questionDiv = document.getElementById("questions-div");
 var choice = document.getElementById("choices");
+let viewScoresBtn = document.getElementById("view-scores");
 let storedArray = JSON.parse(window.localStorage.getItem("highScores"));
-let viewScoresBtn = document.getElementById("view-scores")
 let scoresDiv = document.getElementById("scores-div");
 let results = document.getElementById("results");
-var choice = document.getElementById("choices");
+startButton.addEventListener("click", setTime);
 var quizCount = 0;
 let score = 0;
+let emptyArray = [];
 
-
+// timer function to count down
 function setTime() {
     displayQuestions();
     let timerInterval = setInterval(function() {
@@ -29,21 +29,27 @@ function setTime() {
   }
 
   function displayQuestions() {
+    // removes start button when clicked and continues to quiz questions
     removeEls(startButton);
   
     if (quizCount < quiz.length) {
-      questionDiv.innerHTML = quiz[quizCount].title;
+      questionDiv.innerHTML = quiz[quizCount].Question;
       choice.textContent = "";  
-      for (let i = 0; i < quiz[quizCount].multiChoice.length; i++) {
+      for (let i = 0; i < quiz[quizCount].Choices.length; i++) {
         let el = document.createElement("button");
-        el.innerText = quiz[quizCount].multiChoice[i];
+        // creates buttons for each choice
+        el.innerText = quiz[quizCount].Choices[i];
         el.setAttribute("data-id", i);
+        // adds each question and choice to the page
         el.addEventListener("click", function (event) {
+          // stops click event from bubbling up to the parent element
           event.stopPropagation();
-            if (el.innerText === quiz[quizCount].answer) {
+          if (el.innerText === quiz[quizCount].answer) {
+            // takes score and adds time left to it for score
             score += secondsLeft;
           } else {
             score -= 75;
+            // removes 15 seconds from time left if wrong answer
             secondsLeft = secondsLeft - 15;
           }
             questionDiv.innerHTML = "";
@@ -54,20 +60,27 @@ function setTime() {
             displayQuestions();
           }
         });
+        // appends choices to the page from the quiz array in the order they are in
         choice.append(el);
       }
     }
   }
+
   // capture user score and store in local storage
   function captureUserScore() {
+    // stops timer 
     timer.remove();
     choice.textContent = "";
+    // creates input for user initials
     let initialsInput = document.createElement("input");
+    // creates postscore button
     let postScoreBtn = document.createElement("input");
+    // informes user of their score and asks them to enter their initials
     results.innerHTML = `Your score is ${score} Enter initials: `;
     initialsInput.setAttribute("type", "text");
     postScoreBtn.setAttribute("type", "button");
     postScoreBtn.setAttribute("value", "Post Score!");
+    // reads stored scores from local storage if no score just adds new score, if scores then appends new score to list
     postScoreBtn.addEventListener("click", function (event) {
       event.preventDefault();
       let scoresArray = defineScoresArray(storedArray, emptyArray);
@@ -77,21 +90,27 @@ function setTime() {
         score: score,
       };  
       scoresArray.push(userAndScore);
+      // sends scoresArray to saveScores function
       saveScores(scoresArray);
+      // calls to goBackBtn function to create go back button
       goBackBtn();
+      // calls to clearScoresBtn function to create clear scores button
       clearScoresBtn();
+      // calls to viewScores function to create view scores button
       displayAllScores();
       viewScoresBtn.remove();
     });
+    // appends results with itials input and post score button
     results.append(initialsInput);
     results.append(postScoreBtn);
   }
 
+  // funtion to save scores to local storage
   const saveScores = (array) => {
     window.localStorage.setItem("highScores", JSON.stringify(array));
   }
 
-
+// function to define scores array if no scores stored in local storage or if scores stored in local storage
   const defineScoresArray = (arr1, arr2) => {
     if(arr1 !== null) {
       return arr1
@@ -99,7 +118,7 @@ function setTime() {
       return arr2
     }
   }
-
+// function to create go back button
   function goBackBtn() {
     let backBtn = document.createElement("input");
     backBtn.setAttribute("type", "button");
@@ -110,12 +129,41 @@ function setTime() {
     })
     buttonsDiv.append(backBtn)
   }
+  // function to create view scores button
+    function viewScores() {
+      viewScoresBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        // timer and start button from window
+        removeEls(timer, startButton);
+        // calls to displayallScores function to create view scores button
+        displayAllScores();
+        // removes elements viewcoresbutton
+        removeEls(viewScoresBtn);
+        // calls to clearScoresButton Function to create clear scores button
+        clearScoresBtn();
+        // calls to goBackBtn function to create go back button
+        goBackBtn();
+      });
+    }
+  // function to create clear scores button
+    function clearScoresBtn() {    
+      let clearBtn = document.createElement("input");
+      clearBtn.setAttribute("type", "button");
+      clearBtn.setAttribute("value", "Clear Scores");
+      clearBtn.addEventListener("click", function(event){
+        event.preventDefault();
+        removeEls(scoresDiv);
+        // removes itesm from local storage in highscores
+        window.localStorage.removeItem("highScores");
+      })
+      scoresDiv.append(clearBtn)
+    }
 
 // removes all child elements
   const removeEls = (...els) => {
     for (let el of els) el.remove();
   }
-
+// fucntion to display all scores
   function displayAllScores() {
     removeEls(timer, startButton, results);
     let scoresArray = defineScoresArray(storedArray, emptyArray);
@@ -123,63 +171,47 @@ function setTime() {
       let initials = obj.initials;
       let storedScore = obj.score;
       let resultsP = document.createElement("p");
-      resultsP.innerText = `${initials}: ${storedScore}`;
       scoresDiv.append(resultsP);
+      resultsP.innerText = `${initials}: ${storedScore}`;
     });
   }
-
-  function viewScores() {
-    viewScoresBtn.addEventListener("click", function(event) {
-      event.preventDefault();
-      removeEls(timer, startButton);
-      displayAllScores();
-      removeEls(viewScoresBtn);
-      clearScoresBtn();
-      goBackBtn();
-    });
-  }
-
-  function clearScoresBtn() {    
-    let clearBtn = document.createElement("input");
-    clearBtn.setAttribute("type", "button");
-    clearBtn.setAttribute("value", "Clear Scores");
-    clearBtn.addEventListener("click", function(event){
-      event.preventDefault();
-      removeEls(scoresDiv);
-      window.localStorage.removeItem("highScores");
-    })
-    scoresDiv.append(clearBtn)
-  }
-
-
+// calls to viewScores function on load of page to view scores
+    viewScores();
+// idea of use of this style of quiz variables https://www.sitepoint.com/simple-javascript-quiz/
   var quiz = [
     {
-      title: "What language specifies the style of elements on a webpage?",
-      multiChoice: ["HTML", "Javascript", "Cascading Style Sheets", "Typescript"],
+      Question: "What language specifies the style of elements on a webpage?",
+      Choices: ["HTML", "Javascript", "Cascading Style Sheets", "Typescript"],
       answer: "Cascading Style Sheets"
     },
   
     {
-      title: "Which tag element is not visible from a browser view",
-      multiChoice: ["List Items", "Title", "Paragraph", "Style"],
-      answer: "Style"
+      Question: "Which tag element is not visible from a browser view",
+      Choices: ["<li>items</li>", "<h1>Title</h1>", "<p>paragraph</p>", "<br> break"],
+      answer: "<br> break"
     },
   
     {
-      title: "How can you make a numbered List",
-      multiChoice: [ "<li>", "<ol>", "<ul>", "<dl>"],
+      Question: "How can you make a numbered List",
+      Choices: [ "<li>", "<ol>", "<ul>", "<dl>"],
       answer: "<ol>"
     },
   
     {
-      title: "which array method removes the last element from an array",
-      multiChoice: [".pop()", ".push()", ".length", ".join()"],
+      Question: "which array method removes the last element from an array",
+      Choices: [".pop()", ".push()", ".length", ".join()"],
       answer: ".pop()"
     },
   
     {
-      title: "what is 2+2?",
-      multiChoice: ["no one knows", "its uncalculatable", "its 4 of course", "come on im not a scientist"],
-      answer: "its 4 of course"
+      Question: "How do you create a function in JavaScript?",
+      Choices: ["function = myFunction()", "Function myFunction()", "function:myFunction()", "function+=myFunction()"],
+      answer: "Function myFunction()"
+    },
+
+    {
+      Question: "How do you round the number 7.25, to the nearest integer?",
+      Choices: ["round(7.25)", "Math.round(7.25)", "Math.floor(7.25)", "Math.ceil(7.25)"],
+      answer: "Math.round(7.25)"
     }
   ];
